0c4cbc4bf97d83972ae54f1e8092327e339119bb
sabiqaz <sabiqandazhar@gmail.com>
2023-02-07 23:36:48 +0700

    adding follow/unfollow controller

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 const router = require('express').Router()
 const bodyParser = require('body-parser')
 
-const {getUser, deleteUser, updateUser} = require('../controllers/users')
+const {getUser, deleteUser, updateUser, userFollow, userUnfollow} = require('../controllers/users')
 
 const jsonParser = bodyParser.json()
 
 router.get("/:id", getUser)
 router.put("/:id", jsonParser, updateUser)
+router.put("/:id/follow", jsonParser, userFollow)
+router.put("/:id/unfollow", jsonParser, userUnfollow)
 router.delete("/:id", jsonParser, deleteUser)
 
 module.exports = router
